1920727071976a5f25c85405f806f7789d138f82
MinhTB <minhtran21301@gmail.com>
2020-12-06 22:11:11 +0700

    add chart_bar widget

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -25,7 +25,10 @@ class Chart extends StatelessWidget {
         }
         print(DateFormat.E().format(weekDay));
         print(totalSum);
-        return {'day': DateFormat.E().format(weekDay), 'amount': totalSum};
+        return {
+          'day': DateFormat.E().format(weekDay).substring(0, 1),
+          'amount': totalSum,
+        };
       },
     );
   }
@@ -37,7 +40,9 @@ class Chart extends StatelessWidget {
       elevation: 6,
       margin: EdgeInsets.all(6),
       child: Row(
-        children: <Widget>[],
+        children: groupedTransactionValues.map((data) {
+          return Text("${data['day']}: ${data['amount']}");
+        }).toList(),
       ),
     );
   }
