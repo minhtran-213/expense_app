033b0071659fe9a62bfd7042c8dbf85fb5edd2cd
MinhTB <minhtran21301@gmail.com>
2020-12-09 09:13:43 +0700

    change height dynamically

--- lib/main.dart
+++ lib/main.dart
@@ -53,10 +53,15 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _transaction_list = [];
   List<Transaction> get _recentTransactions {
-    return _transaction_list.where((transaction) {
-      return transaction.date
-          .isAfter(DateTime.now().subtract(Duration(days: 7)));
-    }).toList();
+    return _transaction_list.where(
+      (transaction) {
+        return transaction.date.isAfter(
+          DateTime.now().subtract(
+            Duration(days: 7),
+          ),
+        );
+      },
+    ).toList();
   }
 
   void _addNewTransaction(String title, double amount, DateTime chosenDate) {
@@ -67,9 +72,11 @@ class _MyHomePageState extends State<MyHomePage> {
       id: DateTime.now().toString(),
     );
 
-    setState(() {
-      _transaction_list.add(newTransaction);
-    });
+    setState(
+      () {
+        _transaction_list.add(newTransaction);
+      },
+    );
   }
 
   void _deleteTransaction(String id) {
@@ -80,38 +87,51 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _startAddNewTransaction(BuildContext context) {
     showModalBottomSheet(
-        context: context,
-        builder: (_) {
-          return GestureDetector(
-            onTap: () {},
-            child: NewTransaction(_addNewTransaction),
-            behavior: HitTestBehavior.opaque,
-          );
-        });
+      context: context,
+      builder: (_) {
+        return GestureDetector(
+          onTap: () {},
+          child: NewTransaction(_addNewTransaction),
+          behavior: HitTestBehavior.opaque,
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter App'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.add,
-              color: Colors.white,
-            ),
-            onPressed: () => _startAddNewTransaction(context),
+    var appBar2 = AppBar(
+      title: Text('Flutter App'),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.add,
+            color: Colors.white,
           ),
-        ],
-      ),
+          onPressed: () => _startAddNewTransaction(context),
+        ),
+      ],
+    );
+    return Scaffold(
+      appBar: appBar2,
       body: SingleChildScrollView(
         child: Column(
           //mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Chart(_transaction_list),
-            TransactionsList(_recentTransactions, _deleteTransaction),
+            Container(
+                height: (MediaQuery.of(context).size.height -
+                        appBar2.preferredSize.height -
+                        MediaQuery.of(context).padding.top) *
+                    0.5,
+                child: Chart(_transaction_list)),
+            Container(
+                height: (MediaQuery.of(context).size.height -
+                        appBar2.preferredSize.height -
+                        MediaQuery.of(context).padding.top) *
+                    0.5,
+                child:
+                    TransactionsList(_recentTransactions, _deleteTransaction)),
           ],
         ),
       ),
--- lib/widgets/transaction_list_widget.dart
+++ lib/widgets/transaction_list_widget.dart
@@ -10,58 +10,54 @@ class TransactionsList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 700,
-      child: transactions.isEmpty
-          ? Column(
-              children: <Widget>[
-                Text(
-                  'No transaction added yet!',
-                  style: Theme.of(context).textTheme.title,
+    return transactions.isEmpty
+        ? Column(
+            children: <Widget>[
+              Text(
+                'No transaction added yet!',
+                style: Theme.of(context).textTheme.title,
+              ),
+              SizedBox(height: 50),
+              Container(
+                height: 200,
+                child: Image.asset(
+                  'assets/images/waiting.png',
+                  fit: BoxFit.cover,
                 ),
-                SizedBox(height: 50),
-                Container(
-                  height: 200,
-                  child: Image.asset(
-                    'assets/images/waiting.png',
-                    fit: BoxFit.cover,
+              )
+            ],
+          )
+        : ListView.builder(
+            itemBuilder: (ctx, index) {
+              return Card(
+                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+                child: ListTile(
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    color: Theme.of(context).errorColor,
+                    onPressed: () => removeTransactions(transactions[index].id),
                   ),
-                )
-              ],
-            )
-          : ListView.builder(
-              itemBuilder: (ctx, index) {
-                return Card(
-                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 10),
-                  child: ListTile(
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      color: Theme.of(context).errorColor,
-                      onPressed: () =>
-                          removeTransactions(transactions[index].id),
-                    ),
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                        padding: EdgeInsets.all(6),
-                        child: Container(
-                          height: 20,
-                          child: FittedBox(
-                              child: Text("\$${transactions[index].amount}")),
-                        ),
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: Padding(
+                      padding: EdgeInsets.all(6),
+                      child: Container(
+                        height: 20,
+                        child: FittedBox(
+                            child: Text("\$${transactions[index].amount}")),
                       ),
                     ),
-                    title: Text(
-                      transactions[index].title,
-                      style: Theme.of(context).textTheme.title,
-                    ),
-                    subtitle: Text(
-                        DateFormat.yMMMd().format(transactions[index].date)),
                   ),
-                );
-              },
-              itemCount: transactions.length,
-            ),
-    );
+                  title: Text(
+                    transactions[index].title,
+                    style: Theme.of(context).textTheme.title,
+                  ),
+                  subtitle:
+                      Text(DateFormat.yMMMd().format(transactions[index].date)),
+                ),
+              );
+            },
+            itemCount: transactions.length,
+          );
   }
 }
